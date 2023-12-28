c2a18af7f23595ab53724737408e7c8d70964254
JaMaLQ30 <149305921+JaMaLQ30@users.noreply.github.com>
2023-12-28 17:03:35 +0300

    from the past to GAME

---
+++ Assets/Scenes/SceneSwitcher.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public string targetSceneName; // «”„ «·”Ì‰ «·–Ì  —€» ›Ì «·«‰ ﬁ«· ≈·ÌÂ
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(targetSceneName); //  Õ„Ì· «·”Ì‰ «·ÃœÌœ
+    }
+}
