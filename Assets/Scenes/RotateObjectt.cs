b94cf662cb301f419a645cb8b5dce9444dc1d365
KkkXxx1988 <144284472+KkkXxx1988@users.noreply.github.com>
2024-01-06 00:49:20 +0300

    good

---
+++ Assets/Scenes/RotateObjectt.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  RotateObjectt : MonoBehaviour
+{
+    public int RotateSpeed = -1;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, RotateSpeed, 0, Space.World);
+    }
+}
