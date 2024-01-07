0887624396f2af43607e9587b26e48476178c3a9
Asmaabood18 <124885131+Asmaabood18@users.noreply.github.com>
2024-01-07 16:26:48 +0300

    THE LAST7

---
+++ Assets/Scenes/ASOM/UI/load c#/AS9.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AS9 : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(9);
+    }
+}
